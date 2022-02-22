ca8180f2d668ad4a8d8327daa4ba5a0bc86d1fca
Robert <84203515+itskaru@users.noreply.github.com>
2022-02-22 14:48:13 +0100

    Update proyecto_datos.py

--- proyecto_datos.py
+++ proyecto_datos.py
@@ -3,12 +3,12 @@ from funciones_proyecto_datos import *
 #PRINCIPAL:
 #Menu para introducir respuesta.
 respuesta_info = raw_input(
-"==============\nPara buscar informacion pulse: [1].\nPara aportar nuevos datos introduzca: [2].\nPara obtener otra informacion introduzca: [3].\n==============\nRespuesta: ")
-    
+    "==============\nPara buscar informacion pulse: [1].\nPara aportar nuevos datos introduzca: [2].\nPara obtener otra informacion introduzca: [3].\n==============\nRespuesta: ")
+
 #Buscar datos:
 if respuesta_info == "1":
     buscar_datos()
-    
+
 #Introducir datos:
 elif respuesta_info == "2":
     print "=============="
@@ -19,15 +19,15 @@ elif respuesta_info == "2":
         introducir_apellido()
         introducir_correo()
         introducir_contrasena()
-        condiciones_ciclo = confirmar_datos()   
+        condiciones_ciclo = confirmar_datos()
 
-    guardar_datos()  
-        
-    
+    guardar_datos()
+
+
 #Mas informacion:
 elif respuesta_info == "3":
     print "==============\n<> PROYECTO CREADO POR: itskaru_\n=============="
-    
+
 #Respuesta dada al no ser reconocida la respuesta.
 else:
     print "=============="
